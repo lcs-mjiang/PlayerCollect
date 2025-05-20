bbc1665ce4d403610eb96f94522e63fef4ad3c02
Michael Jiang <mjiang@lcs.on.ca>
2025-05-20 11:45:48 -0400

    The basic framework structure of ItemView

--- Views/ItemView.swift
+++ Views/ItemView.swift
@@ -5,3 +5,18 @@
 //  Created by Michael on 2025/5/20.
 //
 
+import SwiftUI
+
+struct ItemView: View {
+    let player: PlayerItem
+
+    var body: some View {
+        VStack {
+            Text(player.name).font(.largeTitle)
+            Text("Country: \(player.country)")
+            Text("Rank: \(player.rank)")
+        }
+        .padding()
+        .navigationTitle(player.name)
+    }
+}
