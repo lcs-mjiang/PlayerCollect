6455f8433b918c51f02b2afb181cbb2b0267c176
Michael Jiang <mjiang@lcs.on.ca>
2025-05-20 11:26:45 -0400

    Initial Commit

---
+++ PlayerCollect/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PlayerCollect
+//
+//  Created by Michael on 2025/5/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PlayerCollect/PlayerCollectApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlayerCollectApp.swift
+//  PlayerCollect
+//
+//  Created by Michael on 2025/5/20.
+//
+
+import SwiftUI
+
+@main
+struct PlayerCollectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
