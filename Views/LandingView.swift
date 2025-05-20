473452adb023f10ab9682c4a9e36716251612c62
Michael Jiang <mjiang@lcs.on.ca>
2025-05-20 11:36:54 -0400

    The basic framework of LandingView's code

---
+++ Views/LandingView.swift
@@ -0,0 +1,14 @@
+//
+//  LandingView.swift
+//  PlayerCollect
+//
+//  Created by Michael on 2025/5/20.
+//
+
+import SwiftUI
+
+struct LandingView: View {
+    var body: some View {
+        Text("Landing Page")
+    }
+}
